9c36d9b8d1a28c4e8a9f5ae3202009a82338b10e
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2022-08-02 11:12:54 +0000

    Creating new service economic-crime-levy-returns

---
+++ app/uk/gov/hmrc/economiccrimelevyreturns/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.economiccrimelevyreturns.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
