abd3c9bc20aa995b54078ab361895f137c5080c2
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2022-09-26 14:29:48 +0100

    Use correct version of scalacheck

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,10 +8,10 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28"  % "7.4.0",
-    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-28" % "0.73.0",
-    "org.mockito"         %% "mockito-scala"           % "1.17.12",
-    "org.scalacheck"      %% "scalacheck"              % "1.17.0"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28"   % "7.4.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"  % "0.73.0",
+    "org.mockito"       %% "mockito-scala"            % "1.17.12",
+    "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
