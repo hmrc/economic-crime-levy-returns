cacc330b80d81191c97b5f796526c0cbc53c24d3
TonyCOgilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-01-04 13:39:20 +0000

    Cached implicit arbitraries and copyright updated

---
+++ test-common/uk/gov/hmrc/economiccrimelevyreturns/generators/CachedArbitraries.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.economiccrimelevyreturns.generators
+
+import com.danielasfregola.randomdatagenerator.RandomDataGenerator.derivedArbitrary
+import org.scalacheck.Arbitrary
+import org.scalacheck.derive.MkArbitrary
+import uk.gov.hmrc.economiccrimelevyreturns.EclTestData
+import uk.gov.hmrc.economiccrimelevyreturns.models.des.{ObligationData, ObligationStatus}
+import uk.gov.hmrc.economiccrimelevyreturns.models.integrationframework.FinancialDetails
+
+object CachedArbitraries extends EclTestData {
+
+  private def mkArb[T](implicit mkArb: MkArbitrary[T]): Arbitrary[T] = MkArbitrary[T].arbitrary
+
+  implicit lazy val arbOptObligationData: Arbitrary[Option[ObligationData]] = mkArb
+  implicit lazy val arbFinancialDetails: Arbitrary[FinancialDetails]        = mkArb
+  implicit lazy val arbObligationStatus: Arbitrary[ObligationStatus]        = mkArb
+
+}
