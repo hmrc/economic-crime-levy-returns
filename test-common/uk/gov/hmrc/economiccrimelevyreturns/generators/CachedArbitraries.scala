e8d8fd31c99378ebb4bc2051932a00afbe6c68ce
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2023-03-24 11:02:13 +0000

    ECL-297: Removed rest of migrated code

--- test-common/uk/gov/hmrc/economiccrimelevyreturns/generators/CachedArbitraries.scala
+++ test-common/uk/gov/hmrc/economiccrimelevyreturns/generators/CachedArbitraries.scala
@@ -20,7 +20,7 @@ import com.danielasfregola.randomdatagenerator.RandomDataGenerator.derivedArbitr
 import org.scalacheck.Arbitrary
 import org.scalacheck.derive.MkArbitrary
 import uk.gov.hmrc.economiccrimelevyreturns.EclTestData
-import uk.gov.hmrc.economiccrimelevyreturns.models.{CalculateLiabilityRequest, CalculatedLiability}
+import uk.gov.hmrc.economiccrimelevyreturns.models.CalculatedLiability
 import uk.gov.hmrc.economiccrimelevyreturns.models.des.ObligationStatus
 import uk.gov.hmrc.economiccrimelevyreturns.models.integrationframework.{EclReturnDetails, SubmitEclReturnResponse}
 
@@ -28,10 +28,9 @@ object CachedArbitraries extends EclTestData with Generators {
 
   private def mkArb[T](implicit mkArb: MkArbitrary[T]): Arbitrary[T] = MkArbitrary[T].arbitrary
 
-  implicit lazy val arbObligationStatus: Arbitrary[ObligationStatus]                   = mkArb
-  implicit lazy val arbCalculatedLiability: Arbitrary[CalculatedLiability]             = mkArb
-  implicit lazy val arbCalculateLiabilityRequest: Arbitrary[CalculateLiabilityRequest] = mkArb
-  implicit lazy val arbSubmitEclReturnResponse: Arbitrary[SubmitEclReturnResponse]     = mkArb
-  implicit lazy val arbEclReturnDetails: Arbitrary[EclReturnDetails]                   = mkArb
+  implicit lazy val arbObligationStatus: Arbitrary[ObligationStatus]               = mkArb
+  implicit lazy val arbCalculatedLiability: Arbitrary[CalculatedLiability]         = mkArb
+  implicit lazy val arbSubmitEclReturnResponse: Arbitrary[SubmitEclReturnResponse] = mkArb
+  implicit lazy val arbEclReturnDetails: Arbitrary[EclReturnDetails]               = mkArb
 
 }
