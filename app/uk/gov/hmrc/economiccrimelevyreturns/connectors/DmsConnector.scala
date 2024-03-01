f52179dba4c77681cb5c5b2d65adc85da0d65f49
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-03-01 15:58:17 +0000

    Temp Upgrade to Play 3.0

--- app/uk/gov/hmrc/economiccrimelevyreturns/connectors/DmsConnector.scala
+++ app/uk/gov/hmrc/economiccrimelevyreturns/connectors/DmsConnector.scala
@@ -16,10 +16,10 @@
 
 package uk.gov.hmrc.economiccrimelevyreturns.connectors
 
-import akka.NotUsed
-import akka.actor.ActorSystem
-import akka.stream.scaladsl.Source
-import akka.util.ByteString
+import org.apache.pekko.NotUsed
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.stream.scaladsl.Source
+import org.apache.pekko.util.ByteString
 import com.typesafe.config.Config
 import play.api.http.HeaderNames.AUTHORIZATION
 import play.api.http.Status.ACCEPTED
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,25 +6,26 @@ object AppDependencies {
   private val hmrcMongoVersion     = "1.7.0"
   private val openHtmlToPdfVersion = "1.0.10"
 
+
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"    % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"           % hmrcMongoVersion,
     "com.openhtmltopdf"  % "openhtmltopdf-pdfbox"         % openHtmlToPdfVersion,
     "org.typelevel"     %% "cats-core"                    % "2.10.0",
     "io.circe"          %% "circe-json-schema"            % "0.2.0",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-28" % "1.6.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "1.10.0",
     "io.circe"          %% "circe-parser"                 % "0.14.6",
     "com.beachape"      %% "enumeratum-play-json"         % "1.8.0"
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % hmrcBootstrapVersion,
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.mockito"          %% "mockito-scala"           % "1.17.30",
-    "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.18.0",
-    "com.danielasfregola"  %% "random-data-generator"   % "2.9",
-    "io.circe"             %% "circe-json-schema"       % "0.2.0",
-    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
+    "org.mockito"          %% "mockito-scala"            % "1.17.30",
+    "org.scalatestplus"    %% "scalacheck-1-17"          % "3.2.18.0",
+    "com.danielasfregola"  %% "random-data-generator"    % "2.9",
+    "io.circe"             %% "circe-json-schema"        % "0.2.0",
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"    % "1.1.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
